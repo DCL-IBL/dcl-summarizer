89e0d16f9e0b1b0b095ba4fc848a7354ef159217
Jordan Kralev <jkralev@ieee.org>
2025-03-07 03:02:38 -0600

    update links for containers and add clear db

--- backend/src/routes/pdfRoutes.js
+++ backend/src/routes/pdfRoutes.js
@@ -8,5 +8,6 @@ const upload = multer({ dest: 'uploads/' });
 router.post('/process-pdf', upload.single('pdfFile'), pdfController.processPdf);
 router.post('/process-txt', upload.single('txtFile'), pdfController.processTxt);
 router.post('/process-rag', pdfController.getRAGQueryResponse);
+router.post('/clear-db', pdfController.clearDB);
 
 module.exports = router;
--- backend/src/services/documentProcessor.js
+++ backend/src/services/documentProcessor.js
@@ -2,8 +2,11 @@ const { TextLoader } = require("langchain/document_loaders/fs/text");
 const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
 const { Chroma } = require("@langchain/community/vectorstores/chroma");
 const { OllamaEmbeddings } = require("@langchain/community/embeddings/ollama");
+const { ChromaClient } = require("chromadb");
 
 const OLLAMA_URL = process.env.OLLAMA_URL || 'http://localhost:11434';
+const MODEL_EMB = process.env.MODEL_EMB
+const CHROMA_URL = process.env.CHROMA_URL
 
 exports.embeddingsTextDocument = async function(filePath) {
     // Load text document
@@ -19,17 +22,24 @@ exports.embeddingsTextDocument = async function(filePath) {
     // Split and store documents
     const splitDocs = await splitter.splitDocuments(docs);
         
-    const embeddings = new OllamaEmbeddings({ baseUrl: OLLAMA_URL, model: "deepseek-r1" });
+    const embeddings = new OllamaEmbeddings({ baseUrl: OLLAMA_URL, model: MODEL_EMB });
     
     const vectorStore = new Chroma(
       embeddings,
       { 
         collectionName: "text_docs",
-        url: "http://chromadb:8000" 
+        url: CHROMA_URL 
       }
     );
     await vectorStore.addDocuments(splitDocs);
   
     return vectorStore;
   }
+
+exports.deleteTextEmbeddings = async function(collection_name) {
+    const client = new ChromaClient({ path: CHROMA_URL });
+    const collection = await client.getCollection({ name: collection_name });
+    await client.deleteCollection(collection);
+    return 0;    
+}
 
