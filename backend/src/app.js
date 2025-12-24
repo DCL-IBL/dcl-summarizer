26a18be8e47c49e08ff53227ff57c3737923f283
Yordan Kralev <jkralev@milacares.com>
2025-12-24 18:48:31 +0200

    comment static html

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,13 +10,14 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
+/*
 app.use(express.static('/html'));
 app.use((req, res, next) => {
   if (req.method === "GET" && !req.url.startsWith("/api")) {
     return res.sendFile("/html/index.html");
   }
   next();
-});
+});*/
 
 app.use('/api', pdfRoutes);
 app.use('/auth',authRoutes);
